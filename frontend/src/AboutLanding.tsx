902e6514fa1882b70248498c55a79295894b70d7
El1teCoder <78827027+E1iteCoder@users.noreply.github.com>
2024-01-18 13:48:23 -0600

    Delete frontend/src/AboutLanding.tsx
    Already moved this to its subsequent folder

--- frontend/src/AboutLanding.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/AboutLanding.css";
-export default function Landing(): JSX.Element {
-  return (
-    <div className="landing">
-      <h1 className="overlay_text">About Us</h1>
-    </div>
-  );
-}
