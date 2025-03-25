e6bf303538ae58288a0ae731e97a3cd36ee1e605
El1teCoder <78827027+E1iteCoder@users.noreply.github.com>
2025-03-25 13:47:51 -0500

    Update Filter.tsx
    Removed bootstrap

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Form } from "react-bootstrap";
 import "../styles/Filter.css";
 
 type FilterProps = {
@@ -14,45 +13,36 @@ export default function Filter({ isOpen }: FilterProps) {
       <p>Filter</p>
       <ul>
         <li>
-          <label htmlFor="filter-1">
-            Genre
-          </label>
-          <Form.Select id="filter-1-1" className="border-secondary">
+          <label htmlFor="filter-1">Genre</label>
+          <datalist id="filter-1-1" className="border-secondary">
             <option></option>
             <option id="filter-1-1">Fan-fiction</option>
             <option id="filter-1-2">Comedy</option>
             <option id="filter-1-3">Family</option>
             <option id="filter-1-4">Mystery</option>
             <option id="filter-1-5">Drama</option>
-          </Form.Select>
+          </datalist>
         </li>
         <li>
-          <label htmlFor="filter-2">
-            Year
-          </label>
+          <label htmlFor="filter-2">Year</label>
           <input type="text" id="filter-2-1" />
         </li>
         <li>
-          <label htmlFor="filter-3">
-            Author
-          </label>
+          <label htmlFor="filter-3">Author</label>
           <input type="text" id="filter-3-1" />
         </li>
         <li>
-          <label htmlFor="filter-4">
-            Type
-          </label>
-          <Form.Select>
+          <label htmlFor="filter-4">Type</label>
+          <datalist>
             <option></option>
             <option id="filter-4-1">All</option>
             <option id="filter-4-2">Article</option>
             <option id="filter-4-3">Blog</option>
             <option id="filter-4-4">Novel</option>
             <option id="filter-4-5">Poem</option>
-          </Form.Select>
+          </datalist>
         </li>
       </ul>
     </div>
   );
 }
-
