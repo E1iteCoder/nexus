8673ae2e3c763e1afffbcbb4a599d0f80a0ee75c
El1teCoder <78827027+E1iteCoder@users.noreply.github.com>
2025-03-28 14:44:30 -0500

    Update MissionStatament.tsx
    Removed bootstrap and used custom react TSX

--- frontend/src/components/MissionStatament.tsx
+++ frontend/src/components/MissionStatament.tsx
@@ -1,30 +1,28 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
 import "../../frontend/styles/Mission.css";
 import MissionCard from "./CardComponent";
 
 export default function MissionStatement(): JSX.Element {
   return (
-    <Container fluid="md" className="pb-5">
-      <Row className="justify-content-center">
-        <Col>
+    <div className="mission-container">
+      <div className="mission-content">
+        <div className="mission-column">
           <MissionCard
             title="Mission Statement"
-            text="Our mission for this company is to enpower the voices of new writers with a platform that is supportive of their creative expressions."
+            text="Our mission for this company is to empower the voices of new writers with a platform that is supportive of their creative expressions."
             image={""}
             alt={""}
           />
-        </Col>
-        <Col>
+        </div>
+        <div className="mission-column">
           <MissionCard
             title="Future Goals"
             text="We hope in the near future to expand this site to others to include their artistic and creative talents and ensure that reading is innovative."
             image={""}
             alt={""}
           />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 }
