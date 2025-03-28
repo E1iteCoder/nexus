068ed45d71cffd7c6ace4d0557fbf8656a6e4ade
El1teCoder <78827027+E1iteCoder@users.noreply.github.com>
2025-03-28 14:43:39 -0500

    Update Hero.tsx
    removed bootstrap and used custom react TSX

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import "../styles/Hero.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 interface HeroProps {
   title: string;
@@ -17,56 +15,16 @@ export default function Hero({
   altText,
 }: HeroProps): JSX.Element {
   return (
-    <Container fluid="md" className="pb-4">
-      <Row>
-        <Col>
-          <div className="text-item">
-            <h1 className="mt-0 landing-header">{title}</h1>
-            <h1 className="mt-3 mb-4 landing-header2">{subtitle}</h1>
-          </div>
-        </Col>
-        <Col>
-          <div className="picture-item">
-            <img src={imageSrc} alt={altText} className="landing-logo" />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="hero-container">
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1 className="landing-header">{title}</h1>
+          <h1 className="landing-header2">{subtitle}</h1>
+        </div>
+        <div className="hero-image">
+          <img src={imageSrc} alt={altText} className="landing-logo" />
+        </div>
+      </div>
+    </div>
   );
 }
-
-/*
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import "../styles/Hero.css";
-import "bootstrap/dist/css/bootstrap.css";
-
-export default function Hero(): JSX.Element {
-  return (
-    <Container fluid="md" className="pb-4">
-      <Row>
-        <Col>
-          <div className="text-item">
-            <h1 className="mt-0 landing-header">Welcome To</h1>
-            <h1 className="mt-3 mb-4 landing-header2">
-              NexusRead<sup>©</sup>
-            </h1>
-            <p className="my-5 mb-3 fs-2 landing-sub-header">
-              The Ever Evolving Content Business
-            </p>
-          </div>
-        </Col>
-        <Col>
-          <div className="picture-item">
-            <img
-              src={"/pictures/NR-logo.png"}
-              alt="NexusRead Main logo"
-              className="landing-logo"
-            />
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
-*/
